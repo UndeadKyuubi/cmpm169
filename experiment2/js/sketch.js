7da9cb44e8c825e19969f9cbd5076848443a7c6a
UndeadKyuubi <91641904+UndeadKyuubi@users.noreply.github.com>
2024-01-22 23:10:01 -0800

    Added experiment 2 to home page

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,67 +1,111 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - User can type with lowercase letters to produce different patterns of sine waves
+// Author: Brandon Jacobson
+// Date: Jan. 22, 2024
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+let lineY;
+let sineWaves = [];
+let sineAmplitude = 50;
+let sineWidth = 20;
+let keyValues = {};
+let lastPressedKey = '';
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+  canvasContainer = $("#canvas-container");
+  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+  canvas.parent("canvas-container");
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  $(window).resize(function() {
+    console.log("Resizing...");
+    resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  })
+
+  stroke(255);
+  strokeWeight(2);
+  lineY = height / 2;
+  
+  keyValues = {
+    'a': 100,
+    'b': 30,
+    'c': 24,
+    'd': 110,
+    'e': 44,
+    'f': 133,
+    'g': 27,
+    'h': 156,
+    'i': 71,
+    'j': 62,
+    'k': 31,
+    'l': 53,
+    'm': 120,
+    'n': 88,
+    'o': 7,
+    'p': 118,
+    'q': 49,
+    'r': 59,
+    's': 166,
+    't': 75,
+    'u': 90,
+    'v': 4,
+    'w': 150,
+    'x': 55,
+    'y': 119,
+    'z': 40,
+  };
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  background(0);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  stroke(255)
+  line(0, lineY, width, lineY);
+  
+  fill(255);
+  textSize(24);
+  textAlign(CENTER, BOTTOM);
+  text(lastPressedKey, width / 2, height - 10);
+
+  for (let i = sineWaves.length - 1; i >= 0; i--) {
+    sineWaves[i].update();
+    sineWaves[i].draw();
+
+    if (sineWaves[i].offScreen()) {
+      sineWaves.splice(i, 1);
+    }
+  }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function keyPressed() {
+    if (keyValues.hasOwnProperty(key)) {
+    sineWaves.push(new SineWave(width, lineY, sineAmplitude, sineWidth, keyValues[key]));
+      lastPressedKey = key;
+  }
+}
+
+class SineWave {
+  constructor(x, lineY, amplitude, width, keyValue){
+    this.x = x;
+    this.lineY = lineY;
+    this.amplitude = amplitude;
+    this.width = width;
+    this.distOnLine = 0;
+    this.speed = 5;
+    this.shift = keyValue;
+  }
+
+  update() {
+    this.x -= this.speed;
+    this.distOnLine += this.speed / this.width;
+  }
+
+  draw() {
+    let y = this.lineY + sin(this.distOnLine) * this.shift;
+    let randomColor = color(random(255), random(255), random(255), random(255));
+    stroke(randomColor);
+    fill(randomColor);
+    ellipse(this.x, y, this.width, this.amplitude);
+  }
+
+  offScreen() {
+    return this.x + this.width < 0;
+  }
 }
