978951558327c1a0c248eba87064de3c02382b7d
UndeadKyuubi <91641904+UndeadKyuubi@users.noreply.github.com>
2024-02-19 17:42:55 -0800

    Added Experiment 6

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,67 +1,106 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - A little game about receiving scary text messages and trying to delete them all in time.
+// Author: Brandon Jacobson
+// Date: Feb. 19th 2024
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+let boxes = [];
+let messageFrequency = 60;
+let scaryMessageProbability = 0.1;
+let maxScaryBoxes = 30;
+let scaryBoxCount = 0;
+let isGameOver = false;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
     canvas.parent("canvas-container");
-    // resize canvas is the page is resized
+
     $(window).resize(function() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+    })
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  background(255);
+  frameRate(30);
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  strokeWeight(3);
+  background(255);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  if (!isGameOver) {
+    if (random() < 1 / messageFrequency) {
+      let x = random(windowWidth - 160);
+      let y = random(windowHeight - 50);
+      let message;
+      let isScary = random() < scaryMessageProbability;
+      if (isScary) {
+        message = random(["DANGER", "RUN AWAY", "WATCH OUT", "BEWARE"]);
+        scaryBoxCount++;
+      } else {
+        message = random(["HELLO", "NICE", "HAPPY", "SMILE"]);
+      }
+      let w = textWidth(message) + 50;
+      let h = 50;
+      let textColor, boxColor;
+      if (isScary) {
+        textColor = color(255, 0, 0);
+        boxColor = color(0);
+        strokeColor = color(0);
+      } else {
+        textColor = color(255);
+        boxColor = color(20, 141, 222);
+        strokeColor = color(20, 141, 222);
+      }
+      let box = { x: x, y: y, w: w, h: h, message: message, isScary: isScary, textColor: textColor, boxColor: boxColor, strokeColor: strokeColor };
+      boxes.push(box);
+    }
+
+    for (let i = 0; i < boxes.length; i++) {
+      let box = boxes[i];
+      let shakingX = box.x;
+      let shakingY = box.y;
+      if (box.isScary) {
+        shakingX += random(-5, 5);
+        shakingY += random(-5, 5);
+      }
+
+      fill(box.boxColor);
+      stroke(box.strokeColor);
+      rect(shakingX, shakingY, box.w, box.h, 50);
+      textSize(30);
+      fill(box.textColor);
+      noStroke();
+      text(box.message, shakingX + 25, shakingY + 35);
+    }
+
+    if (scaryBoxCount >= maxScaryBoxes) {
+      isGameOver = true;
+    }
+
+    if (frameCount % 60 == 0 && messageFrequency > 10) { 
+      messageFrequency -= 4;
+      scaryMessageProbability += 0.04;
+    }
+  } else {
+    background(0);
+    fill(255, 0, 0);
+    textSize(50);
+    textAlign(CENTER, CENTER);
+    text("DO YOU FEEL SAFE?", width / 2, height / 2);
+  }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function mouseClicked() {
+  if (!isGameOver) {
+    for (let i = 0; i < boxes.length; i++) {
+      let box = boxes[i];
+      if (mouseX > box.x && mouseX < box.x + box.w && mouseY > box.y && mouseY < box.y + box.h) {
+        if (box.isScary) {
+          scaryBoxCount--;
+          boxes.splice(i, 1);
+        }
+        break;
+      }
+    }
+  }
 }
